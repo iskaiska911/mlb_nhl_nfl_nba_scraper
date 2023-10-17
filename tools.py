cce1fe229fc5286a9c6c3ef3b4009e4e0fec8623
iskaiska911 <31636303+iskaiska911@users.noreply.github.com>
2023-10-17 21:06:22 +0600

    Update tools.py fix

--- tools.py
+++ tools.py
@@ -4,6 +4,11 @@ import requests
 from loguru import logger as log
 
 def post_products_mlb(base_url,products_formatted):
-    filename=base_url+str(datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")+'.json')
+    shops = ['mlb', 'nfl', 'nhl', 'nba']
+    for shop in shops:
+        if shop in base_url:
+            filename=shop
+
+    filename="results/"+filename+str(datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")+'.json')
     with open(filename, 'w') as f:
         json.dump(products_formatted, f)
